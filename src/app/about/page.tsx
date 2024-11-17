dcf8c7a464e4a3e5ddfa38bb90e7e2afe66dc7d3
RahulB <rahulbisht1012@gmail.com>
2024-11-17 23:19:06 +0530

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,30 @@
+import axios from "axios";
 import React from "react";
 
 const About = () => {
+    (() => {
+        const url = "https://dataroc8.vercel.app/api/events";
+        const headers = {
+            "Content-Type": "application/json",
+            Authorization: "Bearer {{apiKey}}",
+        };
+
+        const eventData = {
+            name: "About Clicked", // *required
+            domain: "project-test-gamma-black.vercel.app", // *required
+            description: "User reached on the about page", // optional
+        };
+
+        const sendRequest = async () => {
+            axios
+            .post(url, eventData, {headers})
+            .then()
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+        };
+        sendRequest();
+    })();
     return <div>About</div>;
 };
 
