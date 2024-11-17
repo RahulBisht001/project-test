9ed1d354c6f2297b2b60ec7dadb45c0d689ceb41
RahulB <rahulbisht1012@gmail.com>
2024-11-18 00:09:03 +0530

    custom event added

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,7 +1,39 @@
+"use client";
+import axios from "axios";
 import React from "react";
 
 const Payment = () => {
-    return <div>Payment</div>;
+    const handleCustomEvent = () => {
+        const url = "https://dataroc8.vercel.app/api/events";
+        const headers = {
+            "Content-Type": "application/json",
+            Authorization: "Bearer w1w8roz7wvdqnpuxikv7h",
+        };
+
+        const eventData = {
+            name: "About Clicked", // *required
+            domain: "project-test-gamma-black.vercel.app", // *required
+            description: "User reached on the about page", // optional
+        };
+
+        const sendRequest = async () => {
+            axios
+            .post(url, eventData, {headers})
+            .then()
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+        };
+        sendRequest();
+    };
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <p>Payment Custom Event Testing</p>
+            <button onClick={handleCustomEvent} className="p-4 bg-green-500 text-black">
+                Click Me
+            </button>
+        </div>
+    );
 };
 
 export default Payment;
